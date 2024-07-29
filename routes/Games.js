953d8ee1acd00a4c77c325aef1ac4fe08c60d938
YUVAL MANDLER <yuval.mandler@em.aus.com>
2024-07-29 07:05:01 +0300

    capital

--- routes/Games.js
+++ routes/Games.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const GameModel = require("../models/Game");
 const BetModel = require("../models/Bet");
 const UserModel = require("../models/User");
-const {pointsCalculation} = require("../utils/gamesUtils");
+const { pointsCalculation } = require("../utils/GamesUtils");
 
 router.post('/games/:id/finish', async (req, res) => {
     const game = await GameModel.findById(req.params.id);
